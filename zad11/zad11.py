eb3deed4f28f63bbde5add8ae09c5f46f6685f09
braverma <31651158+braverma@users.noreply.github.com>
2018-03-23 23:57:19 +0300

    Add files via upload

---
+++ zad11/zad11.py
@@ -0,0 +1,12 @@
+﻿w = input('File name:')
+b=[]
+f = open(w, encoding="utf-8") 
+	lines=f.readlines()
+for line in lines:
+	a=re.findall('вып[ьие][юелтвй][штмаоиьсы]?[ьесяий]?[сьяй]?[сяь]?[я]?')
+for p in a:
+	if p not in b:
+		b.append(p)
+		print(p)
+f.close()
+		
